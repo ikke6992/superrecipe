3d9b5ca891d28cd5d8eb0a67372ed98320f257dc
Migats <migats21@outlook.com>
2023-12-21 14:52:42 +0100

    Added application properties for database

--- src/main/java/nl/itvitae/superrecipe/SuperRecipeApplication.java
+++ src/main/java/nl/itvitae/superrecipe/SuperRecipeApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SuperRecipeApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SuperRecipeApplication.class, args);
 	}
-
 }
--- src/test/java/nl/itvitae/superrecipe/SuperRecipeApplicationTests.java
+++ src/test/java/nl/itvitae/superrecipe/SuperRecipeApplicationTests.java
@@ -5,9 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SuperRecipeApplicationTests {
-
 	@Test
 	void contextLoads() {
 	}
-
 }
