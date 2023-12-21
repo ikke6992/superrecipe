7061f453d62be77d80b5970dc78219e1cb2df093
Migats <migats21@outlook.com>
2023-12-21 15:58:24 +0100

    Added repositories

---
+++ src/main/java/nl/itvitae/superrecipe/repo/UserRepo.java
@@ -0,0 +1,9 @@
+package nl.itvitae.superrecipe.repo;
+
+import nl.itvitae.superrecipe.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepo extends JpaRepository<User, Long> {
+}
