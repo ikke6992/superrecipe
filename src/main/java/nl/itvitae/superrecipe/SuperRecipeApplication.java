c538f2eb62d68ab22d3675414195a784b8a3e81a
Thomas Vrielink <thomasvrielink92@gmail.com>
2023-12-21 13:46:47 +0100

    Generated Spring project

---
+++ src/main/java/nl/itvitae/superrecipe/SuperRecipeApplication.java
@@ -0,0 +1,13 @@
+package nl.itvitae.superrecipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperRecipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperRecipeApplication.class, args);
+	}
+
+}
