f4c55ba5b41e2dc2b18ba995c5f2551a802008e3
TeuschMichael <127761578+TeuschMichael@users.noreply.github.com>
2024-01-24 15:10:16 +0100

    updated login

--- src/main/java/nl/itvitae/superrecipe/repo/UserRepo.java
+++ src/main/java/nl/itvitae/superrecipe/repo/UserRepo.java
@@ -3,5 +3,10 @@ package nl.itvitae.superrecipe.repo;
 import nl.itvitae.superrecipe.model.User;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UserRepo extends CrudRepository<User, Long> {
+
+
+    public Optional<User> findByUsername(String username);
 }
